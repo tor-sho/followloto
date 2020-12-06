5c3b8694a8c9a0da7d70e6ded5a277d596e12494
shoma nakagawa <shoma1210s76@docomo.ne.jp>
2020-12-06 21:16:12 +0900

    12.6
    session destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'homes#create'
+  get '/logout', to: 'homes#destroy'
   get 'twitter/lottery'
   get 'twitter/management'
-  get 'sessions/friendship'
-  get 'sessions/finish'
-  get 'sessions/again'
-
 
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
